433a2a2ff1ba217f0a3a7bc4a905845c1e2a6a33
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2025-01-26 09:05:39 +0330

    Configure URL patterns for authentication

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,13 +1,13 @@
 from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
 
-
+# Custom serializer for user creation, extending Djoser's base user creation serializer
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password',
                   'email', 'first_name', 'last_name']
         ref_name = 'DjoserUserCreateSerializer'
 
-
+# Custom serializer for user representation, extending Djoser's base user serializer
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
