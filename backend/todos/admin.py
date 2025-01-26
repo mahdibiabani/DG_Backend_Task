00f2ddaf5a2954c4cd5873fe91cfff49aef0e56a
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2025-01-26 12:46:42 +0330

    Correct todos fields in admin panel

--- backend/todos/admin.py
+++ backend/todos/admin.py
@@ -3,7 +3,7 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'user', 'completed', 'due_date', 'created_at', 'modified_at')
+    list_display = ('id', 'title', 'user', 'completed', 'due_date', 'created_at', 'updated_at')
     list_filter = ('completed', 'due_date', 'created_at', 'user')  
     search_fields = ('title', 'description')  
     ordering = ('-created_at',)  
