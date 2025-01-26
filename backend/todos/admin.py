526ae35a248885dd90e7eb72e72b796f544ebeb8
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2025-01-26 09:25:48 +0330

    Register task model in admin panel

--- backend/todos/admin.py
+++ backend/todos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'completed', 'due_date', 'created_at', 'modified_at')
+    list_filter = ('completed', 'due_date', 'created_at', 'user')  
+    search_fields = ('title', 'description')  
+    ordering = ('-created_at',)  
