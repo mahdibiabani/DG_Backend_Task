9b1953591c0dd9b20db7759e422cc36cd006707e
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2025-01-26 09:22:14 +0330

    Create task model

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.title
+
+
