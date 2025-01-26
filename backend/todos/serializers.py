d04d526a05d26a74da6b95e6ca1f4fce37240cf1
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2025-01-26 13:13:07 +0330

    make user field read_only

--- backend/todos/serializers.py
+++ backend/todos/serializers.py
@@ -5,4 +5,4 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'  
-        read_only_fields = ['created_at', 'updated_at']  
+        read_only_fields = ['created_at', 'updated_at', 'user']  
